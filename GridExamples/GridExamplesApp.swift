01b477eb37349a8bdf0f949753a5cb8e9e51330f
rileycalkins <90065349+rileycalkins@users.noreply.github.com>
2024-04-26 23:42:52 -0500

    Initial Commit

---
+++ GridExamples/GridExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridExamplesApp.swift
+//  GridExamples
+//
+//  Created by Riley Calkins on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GridExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
